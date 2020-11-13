398393590553478e4c18a684e3add9ddeb3c4a58
Oleg Pykhalov <go.wigust@gmail.com>
2020-11-13 11:58:59 +0300

    src: main: Increase ban time if multiple occurence exists.

--- src/main.py
+++ src/main.py
@@ -24,6 +24,13 @@ MIN_SCORE = 3
 URL_TEMPLATE = 'http://{}.intr/ip-filter'
 FILTER_ACTION = 'setCookie'
 LOG_LEVEL = logging.INFO
+BLOCK_TIME_DEFAULT = 600  # seconds
+BLOCK_TIME_MULTIPLIER = 100  # seconds
+BLOCK_TIME_TTL = 60 * 60 * 4  # seconds
+BLOCK_TIME_TTL_MAX = 7200  # seconds defined in webservices/nginx/lua/filter_api.lua
+
+# parameter, e.g: {"172.16.103.176": {"count": 1, "last_block_time": 1605263045.2532582}, ...}
+bad_guys_remember = {}
 
 
 def get_nginx_hosts():
@@ -74,7 +81,26 @@ def find_bad_guys(es_host, es_index_template, es_query, interval, min_score):
 
 def ban_bad_guys(hosts, addrs, interval, action, url_template):
     if not addrs: return
-    post_data = '\n'.join(('{} {} {}'.format(a, interval, action) for a in addrs)) + '\n'
+
+    def format_post(ip):
+        """
+        Проверить наличие IP адреса в словаре и если есть то увеличить интервал блокировки
+        """
+        global bad_guys_remember
+        if bad_guys_remember and ip in bad_guys_remember.keys():
+            bad_guys_remember[ip]["count"] += 1
+            bad_guys_remember[ip]["last_block_time"] = time.time()
+            increased_interval = interval + bad_guys_remember[ip]["count"] * BLOCK_TIME_MULTIPLIER
+            return '{} {} {}'.format(
+                ip,
+                increased_interval if bad_guys_remember[ip]["count"] > 1 and increased_interval <= BLOCK_TIME_TTL_MAX else interval,
+                action
+            )
+        else:
+            bad_guys_remember[ip] = {"count": 1, "last_block_time": time.time()}
+            return '{} {} {}'.format(ip, interval, action)
+
+    post_data = '\n'.join(map(format_post, addrs)) + '\n'
     with ThreadPoolExecutor(max_workers=len(hosts)) as executor:
         executor.map(lambda p: requests.post(*p), ((url_template.format(h), post_data) for h in hosts))
 
@@ -110,9 +136,15 @@ def main():
         try:
             start = time.time()
             hosts = get_nginx_hosts()
+
+            # Clean up bad_guys_remember.
+            for ip in bad_guys_remember.keys():
+                if (bad_guys_remember[ip]["last_block_time"] < (time.time() - BLOCK_TIME_TTL)):
+                    bad_guys_remember.pop(ip)
+
             bad_guys = find_bad_guys(ES_HOST, ES_INDEX_TEMPLATE, ES_QUERY_CMS_BRUTE, CHECK_INTERVAL, MIN_SCORE)
             bad_guys.extend(find_bad_guys(ES_HOST, ES_INDEX_TEMPLATE, ES_QUERY_CMS_BRUTE, CHECK_INTERVAL * 30, MIN_SCORE))
-            ban_bad_guys(hosts, set((e['address'] for e in bad_guys)), 600, FILTER_ACTION, URL_TEMPLATE)
+            ban_bad_guys(hosts, set((e['address'] for e in bad_guys)), BLOCK_TIME_DEFAULT, FILTER_ACTION, URL_TEMPLATE)
             for each in bad_guys:
                 logging.info('{} added to filters'.format(each['address']), extra=each)
             time.sleep(max(0, CHECK_INTERVAL - (time.time() - start)))
